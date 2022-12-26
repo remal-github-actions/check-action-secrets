4caf7af3af6e00f8c88bcd38caa2fc83b72fa837
Semyon Levin <levin.semen@gmail.com>
2022-12-25 17:30:06 -0800

    test

---
+++ src/internal/types.ts
@@ -0,0 +1,8 @@
+import { components } from '@octokit/openapi-types'
+
+export type Repo = components['schemas']['full-repository']
+
+export type OrgSecret = components['schemas']['organization-actions-secret']
+export type RepoSecret = components['schemas']['actions-secret']
+
+export type ContentDirectory = components['schemas']['content-directory']
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import { context } from '@actions/github'
-import { components } from '@octokit/openapi-types'
 import { newOctokitInstance } from './internal/octokit'
+import { ContentDirectory, OrgSecret, Repo } from './internal/types'
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -12,7 +12,7 @@ const octokit = newOctokitInstance(githubToken)
 
 async function run(): Promise<void> {
     try {
-        const repo = await octokit.repos.get({
+        const repo: Repo = await octokit.repos.get({
             owner: context.repo.owner,
             repo: context.repo.repo,
         }).then(it => it.data)
@@ -22,17 +22,38 @@ async function run(): Promise<void> {
 
         if (repo.owner?.type?.toLowerCase() === 'organization') {
             core.info('Getting organisation secrets')
-            const t = await octokit.paginate(octokit.actions.listOrgSecrets, {
+            const allOrgSecrets: OrgSecret[] = await octokit.paginate(octokit.actions.listOrgSecrets, {
                 org: context.repo.owner,
-            })
-            core.info(JSON.stringify(t, null, 2))
-            const orgSecrets = await octokit.paginate(octokit.actions.listOrgSecrets, {
-                org: context.repo.owner,
-            }).then(it => it.secrets?.map(secret => secret.name))
-            if (orgSecrets != null) allSecretsUnsorted.push(...orgSecrets)
+            }).then(it => it.secrets != null ? it.secrets : it as OrgSecret[])
+            const orgSecrets: OrgSecret[] = []
+            for (const orgSecret of allOrgSecrets) {
+                if (orgSecret.visibility == null || orgSecret.visibility.toLowerCase() === 'all') {
+                    orgSecrets.push(orgSecret)
+                } else if (orgSecret.visibility.toLowerCase() === 'private') {
+                    if (repo.visibility === 'private') {
+                        orgSecrets.push(orgSecret)
+                    }
+                } else if (orgSecret.visibility.toLowerCase() === 'selected') {
+                    const selectedRepoNames = await octokit.actions.listSelectedReposForOrgSecret({
+                        org: context.repo.owner,
+                        secret_name: orgSecret.name,
+                    }).then(it => it.data.repositories.map(that => that.full_name))
+                    if (selectedRepoNames.includes(repo.full_name)) {
+                        orgSecrets.push(orgSecret)
+                    }
+                }
+            }
+            const orgSecretNames = orgSecrets.map(it => it.name)
+            core.info(`Accessible organisation secrets:\n  ${orgSecretNames.join('\n  ')}`)
+            orgSecretNames.forEach(it => allSecretsUnsorted.push(it))
         }
 
         core.info('Getting repository secrets')
+        const t = await octokit.paginate(octokit.actions.listRepoSecrets, {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+        })
+        core.info(JSON.stringify(t, null, 2))
         const repoSecrets = await octokit.paginate(octokit.actions.listRepoSecrets, {
             owner: context.repo.owner,
             repo: context.repo.repo,
@@ -43,16 +64,17 @@ async function run(): Promise<void> {
         core.info(`Accessible secrets:\n  ${allSecrets.join('\n  ')}`)
 
 
-        const workflowFiles: components['schemas']['content-directory'] = await octokit.repos.getContent({
+        const workflowsDir = '.github/workflows'
+        const workflowFiles: ContentDirectory = await octokit.repos.getContent({
             owner: context.repo.owner,
             repo: context.repo.repo,
-            path: '.github/workflows',
+            path: workflowsDir,
             ref,
         }).then(it => it.data as any)
         if (Array.isArray(workflowFiles)) {
             for (const workflowFile of workflowFiles) {
                 if (workflowFile.name.endsWith('.yml')) {
-                    core.info(`Processing ${workflowFile.name}`)
+                    core.info(`Processing ${workflowFile.url}`)
                 }
             }
         }
