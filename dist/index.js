dd55ef3d33cc5d5dedebae4c7e384a6fc4073ec8
remal <remal@users.noreply.github.com>
2022-12-26 02:55:08 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -221,7 +221,12 @@ async function run() {
                             const lines = content.substring(0, pos).split(/\r\n|\n\r|\n|\r/);
                             const line = lines.length;
                             const column = lines[lines.length - 1].length;
-                            if (optionalSecrets.includes(secretName)) {
+                            let isOptional = optionalSecrets.includes(secretName);
+                            if (!isOptional) {
+                                const nextContent = content.substring(pos + secretMatch[0].length);
+                                isOptional = nextContent.trimStart().startsWith('||');
+                            }
+                            if (isOptional) {
                                 core.info(`Optional secret not set: ${secretName}`);
                             }
                             else {
